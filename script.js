a83ad792cf7565d573580f810005e7b7d0dfa266
jdlcrs <j.dlcrs@icloud.com>
2018-09-10 09:06:15 +0200

    add new random button

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
-var body = document.getElementById("gradient");
+var body = document.getElementById("gradient"); 
+var colorSurprise = document.querySelector(".btnRandom");
 
 /* console.log(css, colorOne, colorTwo);*/
 
@@ -10,5 +11,24 @@ function setGradient() {
     css.textContent = body.style.background + ";";
 };
 
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var rand1 = '#', rand2 = '#';
+    for (var i = 0; i < 6; i++) {
+      rand1 += letters[Math.floor(Math.random() * 16)];
+      rand2 += letters[Math.floor(Math.random() * 16)];
+    }
+    return setRandom(rand1, rand2);
+}
+
+function setRandom (num1, num2) {
+    body.style.background = "linear-gradient(to right, " + num1 + "," + num2 + ")";
+    color1.value = num1; color2.value= num2;
+    css.textContent = body.style.background;
+}
+
 color1.addEventListener("input", setGradient);
 color2.addEventListener("input", setGradient);
+
+body.onload = setGradient;
+colorSurprise.onClick = setRandom;
