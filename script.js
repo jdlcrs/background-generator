e4a9a84df55689f67cbbc454ebf2c7a927d1d55e
jdlcrs <jdlcrs@users.noreply.github.com>
2018-09-12 18:16:09 +0200

    upgrade the random button

--- script.js
+++ script.js
@@ -4,35 +4,52 @@ var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient"); 
 var colorSurprise = document.querySelector(".btnRandom");
 
+/* 
+first attempt to get the random button working
+
+function getRandomColor() {
+    var letters = 'ABCDEF', numbers = '0123456789';
+    var rand1 = 'rgb', rand2 = 'rgb';
+    for (var i = 0; i < 6; i++) {
+      rand1 += numbers[Math.floor(Math.random() * 10)];
+      rand2 += numbers[Math.floor(Math.random() * 10)];
+    }
+    return setRandom(rand1, rand2);
+}
+*/
+
+function getRandomRgb() {
+    var num = Math.round(0xffffff * Math.random());
+    var r = num >> 16;
+    var g = num >> 8 & 255;
+    var b = num & 255;
+    return 'rgb(' + r + ', ' + g + ', ' + b + ')';
+}
+
+function getRandomColor() {
+    var rand1 = getRandomRgb(), rand2 = getRandomRgb();
+    return setRandom(rand1, rand2);
+}
+
 function setGradient() {
     body.style.background = "linear-gradient(to right, " + color1.value + "," + color2.value + ")";
     css.textContent = body.style.background + ";";
 };
 
-function getRandomColor() {
-    var letters = '0123456789ABCDEF';
-    var rand1 = '#', rand2 = '#';
-    for (var i = 0; i < 6; i++) {
-      rand1 += letters[Math.floor(Math.random() * 16)];
-      rand2 += letters[Math.floor(Math.random() * 16)];
-    }
-    return setRandom(rand1, rand2);
-}
-
-function setRandom (num1, num2) {
+function setRandom(num1, num2) {
     body.style.background = "linear-gradient(to right, " + num1 + "," + num2 + ")";
-    color1.value = num1; color2.value= num2;
+    color1.style.backgroundColor = num1; color2.style.backgroundColor = num2;
     css.textContent = body.style.background;
 }
 
 color1.addEventListener("input", setGradient);
 color2.addEventListener("input", setGradient);
 
-body.onload = setGradient;
-colorSurprise.addEventListener("click", setRandom);
+body.onload = setGradient();
+colorSurprise.addEventListener("click", getRandomColor);
+
 
 // This is a test function 
-
 function test() {
     console.log("Hello, it's working")
 }
